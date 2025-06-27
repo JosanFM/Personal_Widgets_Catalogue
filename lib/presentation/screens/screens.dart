bd8e38f7319f8ac458c68d018811bc8deda1e3eb
Jose Antonio Fernandez <josan1997fernandez@gmail.com>
2025-06-27 14:35:23 +0200

    Screen creadas y barril export screen actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,6 +1,13 @@
 // La función de este archivo es tener todos los screens para exportarlos al router y no importar de 1 en 1 en el router y tener muchas importaciones
 
+
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
 
